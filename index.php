56677100a4297b2a5dc0360d10a7e79804063d49
Developer Hasan <67751842+DeveloperHasanBD@users.noreply.github.com>
2022-09-07 21:09:02 +0600

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,3 @@
-$designer_cat = wp_get_post_terms(get_the_ID(), 'progetti_cat');
+$designer_cat = wp_get_post_terms(get_the_ID(), 'designer_cat');
 $get_term_id = $designer_cat[0]->term_id;
 $single_page_design_title = get_term_meta($get_term_id, 'single_page_design_title');
