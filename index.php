f31eaca8d49fa895c8e67ad6172229c7eae96f1c
Developer Hasan <67751842+DeveloperHasanBD@users.noreply.github.com>
2022-09-29 12:30:05 +0600

    Update index.php

--- index.php
+++ index.php
@@ -6,3 +6,13 @@ $designer_cat = get_the_terms(get_the_ID(), 'product_cat');
 
 $get_term_id = $designer_cat[0]->term_id;
 $single_page_design_title = get_term_meta($get_term_id, 'single_page_design_title');
+
+
+
+$term_list = wp_get_post_terms(get_the_ID(), 'product_cat', array('parent' => 0));
+foreach ($term_list as $single_term) {
+	$child_list = wp_get_post_terms(get_the_ID(), 'product_cat', array('parent' => $single_term->term_id));
+	foreach ($child_list as $single_child) {
+		print_r($single_term->name . '->' . $single_child->name);
+	}
+}
